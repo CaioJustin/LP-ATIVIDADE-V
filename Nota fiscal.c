6723c5abb731b71a0dcf52e790aabf022e9f452b
Kauã Ferreira <138538625+Kaux97@users.noreply.github.com>
2023-12-16 10:43:24 -0300

    Add files via upload

---
+++ Nota fiscal.c
@@ -0,0 +1,8 @@
+printf("\n==========NOTA FISCAL=========\n");
+printf("Nome do produto: %s",box.nome);
+printf("Quantidade: %d",box.quantidade);
+printf("Valor Total do Produto: %.2f",box.preco);
+printf("\n============================\n");
+printf("\nQuantidade Comprada: %d",quantidade);
+printf("\nPreço Unitário: R$%.2f",(box.preco/quantidade));
+printf("\n=============================\n");
