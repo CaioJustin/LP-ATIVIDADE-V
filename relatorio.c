59c0a5f9cf3d2b0503bc542177d36ef3bf949648
Kauã Ferreira <138538625+Kaux97@users.noreply.github.com>
2023-12-16 10:33:14 -0300

    Add files via upload

---
+++ relatorio.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#define k 3; 
+
+struct controprod{
+    char produto [100];
+    int quantidade;
+    float preco;
+};
+
+int main(){
+int i;
+struct controprod cadp[3];
+
+    printf("==========RELATORIO=========\n");
+        printf("Nome: %s\n", pro.nome);
+        printf("Preço: R$ %.2f\n", pro.preco);
+        printf("Quantidade em estoque: %d\n", pro.estoque);
+        printf("Valor total em estoque: R$ %.2f\n", calcularValor(pro));
+            break;
+
+        
+    }
